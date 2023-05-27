4b892e2b849e97ea2f6ce5086d7ab93351893ed1
GrahamJReid <grahamreidtechnology@gmail.com>
2023-05-27 08:35:37 -0500

    delete event functional

--- pages/events/index.js
+++ pages/events/index.js
@@ -11,13 +11,16 @@ function Home() {
   useEffect(() => {
     getEvents().then((data) => setEvents(data));
   }, []);
+  const displayEvents = () => {
+    getEvents().then((data) => setEvents(data));
+  };
 
   return (
     <><article className="events">
       <h1>Events</h1>
       {events.map((event) => (
         <section key={`event--${event.id}`} className="event">
-          <EventCard id={event.id} game={event.game.title} description={event.description} date={event.date} time={event.time} organizer={event.organizer} />
+          <EventCard id={event.id} game={event.game.title} description={event.description} date={event.date} time={event.time} organizer={event.organizer} onUpdate={displayEvents} />
         </section>
       ))}
       </article>
